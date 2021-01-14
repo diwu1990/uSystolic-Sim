9a2f15b6202f44904dec92331c3ee98d88a1a60b
diwu1990 <diwu0111@hotmail.com>
2021-01-14 11:23:09 -0600

    revert cacti from v7 to v65

--- simHw/cacti_result.py
+++ simHw/cacti_result.py
@@ -94,9 +94,9 @@ def dram_cacti(
     original.close()
     target.close()
 
-    subprocess.call(["make", "all"], shell=True, cwd="./simHw/cacti7/")
+    subprocess.call(["make", "all"], shell=True, cwd="./simHw/cacti65/")
     final_cmd = "./cacti -infile ../../" + target_config_file + " > ../../" + result_file
-    subprocess.call([final_cmd], shell=True, cwd="./simHw/cacti7/")
+    subprocess.call([final_cmd], shell=True, cwd="./simHw/cacti65/")
 
 
 def dram_report_extract(
