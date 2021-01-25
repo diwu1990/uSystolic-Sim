e9fce2e09767b873decd741ff7d48bf83299be3c
diwu1990 <diwu0111@hotmail.com>
2021-01-25 13:34:11 -0600

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -5,8 +5,8 @@ start=`date +%s`
 if ls ./config/; then
     for entry in $(ls ./config/)
     do
-        echo "python3 evaluate.py -name=$entry"
-        python3 evaluate.py -name=$entry &
+        echo "python3 evaluate.py -name=$entry > ${entry}.log"
+        python3 evaluate.py -name=$entry > ${entry}.log &
     done
 else
     echo "No entry in ./config"
