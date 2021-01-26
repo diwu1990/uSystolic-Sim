afb9f61b35d62351e9224c137f347c0a192148e6
diwu1990 <diwu0111@hotmail.com>
2021-01-26 01:50:32 -0600

    update run_all and run_check

--- run_all.sh
+++ run_all.sh
@@ -5,8 +5,10 @@ start=`date +%s`
 if ls ./config/; then
     for entry in $(ls ./config/)
     do
-        echo "python3 evaluate.py -name=$entry > ./log/${entry}.log"
-        python3 evaluate.py -name=$entry > ./log/${entry}.log &
+        if [ $entry != "README.md" ]; then
+            echo "python3 evaluate.py -name=$entry > ./log/${entry}.log"
+            python3 evaluate.py -name=$entry > ./log/${entry}.log &
+        fi
     done
 else
     echo "No entry in ./config"
