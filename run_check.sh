0831af4e15346ee04b283fb2634eec864a96caad
diwu1990 <diwu0111@hotmail.com>
2021-02-07 20:54:41 -0600

    update run_check

--- run_check.sh
+++ run_check.sh
@@ -10,14 +10,14 @@ if ls ./config/; then
                 echo "./outputs/$entry/simArchOut/${entry}_mac_util.csv does not exist!"
             fi
 
-            if [ ! -f "./outputs/$entry/simHwOut/${entry}_area.csv" ]; then
-                echo "./outputs/$entry/simHwOut/${entry}_area.csv does not exist!"
+            if [ ! -f "./outputs/$entry/simEffOut/${entry}_area.csv" ]; then
+                echo "./outputs/$entry/simEffOut/${entry}_area.csv does not exist!"
             fi
-            if [ ! -f "./outputs/$entry/simHwOut/${entry}_energy.csv" ]; then
-                echo "./outputs/$entry/simHwOut/${entry}_energy.csv does not exist!"
+            if [ ! -f "./outputs/$entry/simEffOut/${entry}_energy.csv" ]; then
+                echo "./outputs/$entry/simEffOut/${entry}_energy.csv does not exist!"
             fi
-            if [ ! -f "./outputs/$entry/simHwOut/${entry}_power.csv" ]; then
-                echo "./outputs/$entry/simHwOut/${entry}_power.csv does not exist!"
+            if [ ! -f "./outputs/$entry/simEffOut/${entry}_power.csv" ]; then
+                echo "./outputs/$entry/simEffOut/${entry}_power.csv does not exist!"
             fi
             if [ ! -f "./outputs/$entry/simHwOut/${entry}_detail_ideal.csv" ]; then
                 echo "./outputs/$entry/simHwOut/${entry}_detail_ideal.csv does not exist!"
